af5f94ee77635cfcee9722c56603bb9afdd0a303
anyu <ayu3500@gmail.com>
2017-04-24 19:43:16 -0700

    Set up database connection, create users table

---
+++ server/db/database.js
@@ -0,0 +1,20 @@
+module.exports = (function() {
+  var knex = require('knex')({
+    client: 'mysql',
+    connection: {
+      host : '127.0.0.1',
+      user : 'root',
+      password : '',
+      database : 'tripmate_db'
+    }
+  })
+
+  knex.schema.createTableIfNotExists('users', function (table) {
+    table.increments();
+    table.string('username');
+  }).then(() => {
+    console.log('table created')
+  })
+  return knex;
+})();
+// module.exports = knex;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 var express = require('express')
+var knex = require('knex')
+
+var db = require('./db/database.js')
 var app = express()
 
 app.get('/', function(req, res) {
-  res.send('test')
+  res.send('db', db)
 })
 
 app.listen(3000, function () {
