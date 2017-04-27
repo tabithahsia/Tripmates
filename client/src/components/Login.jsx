7ad720809177f464bc5d4ff584210c4a5ab14906
anyu <ayu3500@gmail.com>
2017-04-26 17:08:41 -0700

    WIP getting login form to submit

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,10 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 import Header from"./Header";
+import axios from 'axios';
+
+
 
 class Login extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      formEntries: {}
+    }
+
+    this.submitLogin = this.submitLogin.bind(this);
+    this.updateInputs = this.updateInputs.bind(this);
+  }
+
+  submitLogin(login) {
+    console.log('got into submit login')
+    axios.post('/login', {username: login.username, password: login.password})
+      .then(({response}) => {
+        console.log(response);
+      })
+      .catch(err => {
+        console.error("error in post entries", err);
+      })
+  }
+
+  updateInputs(e) {
+    var formEntries = this.state.formEntries;
+    var name = e.target.name;
+    console.log(name)
+    var value = e.target.value;
+
+    formEntries[name] = value;
+    this.setState({formEntries});
+
+  }
+
+  submitLoginn() {
+    this.submitLogin(this.state.formEntries)
   }
 
   render() {
@@ -14,15 +50,15 @@ class Login extends React.Component {
         <Header />
 
         <h4>Login Page</h4>
-
-        <form>
+        {console.log('this ', this)}
+        <form onSubmit={this.submitLoginn}>
         <label>
           Username:
-          <input type="text" />
+          <input name="username" type="text" onChange={this.updateInputs}/>
         </label>
         <label>
           Password:
-          <input type="text" />
+          <input name="password" type="text" onChange={this.updateInputs}/>
         </label>
         <input type="submit" value="Submit" />
       </form>
