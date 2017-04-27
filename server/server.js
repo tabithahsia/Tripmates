bb72ed660261e70560850e0919831d6c2c27bb12
Lyddem <lydiademi@gmail.com>
2017-04-26 20:03:26 -0700

    Successfully inserted /signup inputs to database

--- server/server.js
+++ server/server.js
@@ -14,13 +14,24 @@ app.use(express.static(path.join(__dirname, '../client')));
 
 app.post('/login', function (req, res){
   console.log("log in username", req.body.username);
+  var userName = req.body.username;
+  var password = req.body.password;
   res.send(req.body.username)
 })
 
 app.post('/signup', function (req, res){
   console.log("sign up username", req.body.username);
+
+  var userName = req.body.username;
+
+  var query = `INSERT INTO users(username) VALUES ('${userName}')`;
+  db.dbConnection.query(query);
+
   res.send(req.body.username)
 })
+
+
+
 app.listen(3000, function () {
   console.log('Listening on port 3000!')
 })
