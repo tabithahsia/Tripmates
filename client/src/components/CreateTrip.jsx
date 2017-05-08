e882151b0137a2f88563c49b7379cd5ba35841de
Lyddem <lydiademi@gmail.com>
2017-05-08 14:09:22 -0700

    Completed view for CreateTrip component

--- client/src/components/CreateTrip.jsx
+++ client/src/components/CreateTrip.jsx
@@ -11,8 +11,44 @@ class CreateTrip extends React.Component {
     return (
 
       <div>
-        <h1>Create a Trip</h1>
+        <h1 id="createTripHeader">Add a Trip</h1>
+        <h4 id="createTripSubheader">Get Trippy!</h4>
+
+      <form id="createTripParent">
+      <div id="firstHalf">
+          <label>
+            Trip Name <br/>           
+            <input name="tripName" type="text" />
+          </label> <br/><br/>
+
+          <label>
+            Destination <br/>
+            <input name="tripName" type="text" /><br/><br/>
+          </label>
+
+          <label>
+            Date Range <br/>
+            <input name="dateRange" type ="text"/><input type="submit" value="+"/><br/><br/>
+          </label>
       </div>
+      <div id="secondHalf">
+           <label>
+            Estimated Cost <br/>
+            <input name="estimatedCost" type="text" placeholder="$"/><br/><br/>
+          </label>
+
+          <label>
+          Add an Activity <br/>
+            <input name="activity" type ="text" placeholder="Activity name"/><br/><br/>
+            <input name="activity" type ="text" placeholder="Description/Link"/><br/><br/>
+            <input name="activity" type ="text" placeholder="Cost"/><input type="submit" value="+"/><br/><br/>
+          </label>
+      </div> <br/>
+   </form>
+
+          <input type="submit" value="Add a Trip" />       
+      </div>
+
 
     )
   }
