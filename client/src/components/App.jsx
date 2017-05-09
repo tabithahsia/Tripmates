c80d49583af17c1cdb9d5010e731b47df13de9b2
Lyddem <lydiademi@gmail.com>
2017-05-09 12:24:28 -0700

    Created contributeTrip page

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -17,7 +17,8 @@ class App extends React.Component {
           <div id="tagline">Explore the world with your trippy trippy mates</div>
             <Link to="/createTrip"><button id="mainCTA">Create a trip</button></Link>
         </div>
-        <div id="placeholderdiv"></div>
+        <div id="placeholderdiv"><Link to="/contributeTrip"><button id="mainCTA">Contribute to a trip</button></Link></div>
+
       </div>
     )
   }
