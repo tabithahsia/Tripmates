20f6edb2c32feadf281c75f0089d94dafa4cfeaf
Eric Davis <efdavis1@gmail.com>
2017-05-09 16:51:25 -0700

    adds map functionality with trips

--- server/server.js
+++ server/server.js
@@ -19,6 +19,7 @@ app.get('/signup', function (req, res) {
 
 app.get('/profile', function (req, res) {
   var userData = {};
+  var tripArray = [];
 
   var query = `SELECT username FROM logIns ORDER BY id DESC LIMIT 1`;
   db.dbConnection.query(query, function(error,results,fields) {
@@ -33,32 +34,24 @@ app.get('/profile', function (req, res) {
       if(error) {
         console.log(error1)
       }
-      
-      console.log('currentUser', currentUser)
+    
+      var tripsQuery = `SELECT tripName FROM trips INNER JOIN user_trips ON trips.id = user_trips.trip_id WHERE user_id = '${currentUser[0].id}'`
+      db.dbConnection.query(tripsQuery, function (error, tripList, fields) {
+        if (tripList[0]) {
+        console.log('tripList', tripList[0].tripName)
 
-      
-        var tripsQuery = `SELECT tripName FROM trips INNER JOIN user_trips ON trips.id = user_trips.trip_id WHERE user_id = '${currentUser[0].id}'`
-        db.dbConnection.query(tripsQuery, function (error, tripList, fields) {
-          if (tripList[0]) {
-          console.log('tripList', tripList[0].tripName)
-
-          userData['trips'] = tripList[0].tripName;
-          res.send(userData);
-          }
-        })
+        for (var i = 0; i < tripList.length; i++) {
+          tripArray.push(tripList[i].tripName)
+        }
+        userData['trips'] = tripArray;
+        res.send(userData);
+        }
+      })
       
     })
   });
 })
 
-
-
-// app.post('/login', function (req, res){
-//   console.log("log in username", req.body.username);
-//   var userName = req.body.username;
-//   var password = req.body.password;
-//   res.send(req.body.username)
-// })
 
 app.post('/signup', function (req, res){
   console.log("inpost request", req.body);
@@ -95,18 +88,6 @@ app.get('/userTable', function(req, res) {
     res.send(results)
   });
 })
-
-// var addAlbum = function(album, cb) {
-//   var query = `INSERT INTO kanyes(era,year,description,imageURL) VALUES('${album.era}','${album.year}','${album.description}','${album.imageURL}')`;
-//   connection.query(query, (err,results,fields) => {
-//     if (err) {
-//       cb(err, false);
-//     } else {
-//       console.log('Added new album to db');
-//       cb(null, true);
-//     }
-//   });
-// }
 
 
 app.listen(3000, function () {
