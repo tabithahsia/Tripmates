b2c66065755d35b5811f53838fc20773abebcaa7
anyu <ayu3500@gmail.com>
2017-05-11 16:18:54 -0700

    Remove unnecessary imports, 1 styling tweak

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,5 @@ import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 
 import App from './components/App';
 
-import Home from './components/Home';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import Profile from './components/Profile';
-import CreateTrip from './components/CreateTrip';
-import ContributeTrip from './components/ContributeTrip';
-
-
 
 ReactDOM.render(<App />, document.getElementById('app'));
